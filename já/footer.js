5a218332fb9bb02ca556a84b08d3b5fc03111af3
ZicBusiness <77104285+ZicBusiness@users.noreply.github.com>
2023-02-08 08:13:09 -0300

    Delete já directory

--- já/footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-return (
-<footer>
-<div className="footer">Developed by ZicBusiness | Copyright &copy; {new Date().getFullYear()}</div>
-</footer>
-);
-}
-
-ReactDOM.render(<Footer />, document.getElementById("footer_js"));
